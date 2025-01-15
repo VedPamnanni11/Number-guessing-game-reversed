114b9a168b47eaef048b5eb6bb0de916105191cf
Ved Pamnani <152010733+VedPamnanni11@users.noreply.github.com>
2025-01-15 18:10:37 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import random
 
-play_again = 'yes'
+play_again = "yes"
+
+print("Rules: if the computer's guess is bigger than your number then type 'smaller' or if the computer's guess is smaller than your number then type 'bigger' or the computer's guess was correct then type'correct'.")
+print("I bet I can guess your number in less than 10 tries.")
+print("Think of a number between 1 and 100 for the computer to guess")
+
 while play_again == 'yes':
 
     num_guessed = 0
     tries = 0
 
-    print("Rules: if the computer's guess is bigger than your number then type 'smaller' or if the computer's guess is smaller than your number then type 'bigger' or the computer's guess was correct then type'correct'.")
-    print("I bet I can guess your number in less than 10 tries.")
-    print("Think of a number between 1 and 100 for the computer to guess")
-    ready = input("If you are ready to start the game then please type 'start' then press the Enter key! : ").lower()
+    play_again = input("If you are ready to start the game then please type 'yes' then press the Enter key! : ").lower()
 
 
-    if ready == "start":
+    if play_again == "yes":
         low = 1
         high = 100
         while True:
@@ -21,7 +23,7 @@ while play_again == 'yes':
             feedback = input("Is the computer's guess too big or too small or correct? Type 'smaller', 'bigger' or 'correct' : ").lower()
 
             if feedback == "correct":
-                print(f"Yay!, I have guessed your number {num_guessed} in {tries + 1} tries!")
+                play_again = input(f"Yay!, I have guessed your number {num_guessed} in {tries + 1} tries!, Do you want to play again? : ")
                 break
             elif feedback == "smaller":
                 high = num_guessed - 1
